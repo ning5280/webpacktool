b9e8d34a2959b105239f0cf996e3532244ea4542
wangning <529023364@qq.com>
2017-07-19 13:34:00 +0800

    加载通用模块

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
+var webpack = require('webpack');
 module.exports={
-    entry: './src/pages/index/index.js',
+    entry: {
+        'common': ['./src/pages/common/index.js'],
+        'index': ['./src/pages/index/index.js'],
+        'login': ['./src/pages/login/index.js']
+    },
     output: {
         path: './dist',
-        filename: 'bundle.js'
+        filename: 'js/[name].js'
     },
     externals: {
         'jquery' : 'window.jQuery' //将jquery 加载进来 js文件中可以使用 var $$ = require('jquery')使用jquery $$是为了避免全局变量$冲突
-    }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'js/base.js'
+        })
+    ]
 }
